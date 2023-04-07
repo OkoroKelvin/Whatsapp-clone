9910d8c2e8b044537c4340609347f710a5a6e0af
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2023-04-07 12:47:22 +0100

    Use effect

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const add = () =>{
+    setCount(age => age + 3);
+   // setCount(prev => prev +1);
+  }
+
+  const minus = () => {
+    setCount(count - 1)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container} >
+      <SafeAreaView>
+
+        <Button title="Add"  onPress={add}  />
+        <Text style={styles.label} >{count}</Text>
+
+        <Button title="Minus" onPress={minus}/>
+    
+      </SafeAreaView>
+      
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffebcd',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  label:{
+    color: 'black',
+    fontSize: 18
+  }
 });
